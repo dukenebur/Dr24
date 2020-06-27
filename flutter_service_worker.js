425dec6175aca6d9ed26b7f193e39b4b8c2c6223
dukenebur <dukenebur@users.noreply.github.com>
2020-06-27 22:57:14 +0000

    deploy: c585d6a9ed96dc5be1828144151e1c54436a714c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,8 +22,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a33512d825d17c980e61333f89080448",
-"/": "a33512d825d17c980e61333f89080448",
+"index.html": "7afecef2555d44767fcdd3ce519a9fc5",
+"/": "7afecef2555d44767fcdd3ce519a9fc5",
 "main.dart.js": "c4da6331f3247a8eba731eb0095d31d9",
 "manifest.json": "d55aa2e46afcfac3590a4ddf6db6fdb8"
 };
